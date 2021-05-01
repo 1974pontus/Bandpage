b8dcbab48ff4d514cdb9527829caf54680cfc2d4
Pontus Tahr <pontus.tahir@gmail.com>
2021-05-01 10:20:29 +0200

    First commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Startpage from '../Startpage'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Startpage />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Startpage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Startpage = () => {
+    return (
+        <div>
+            <h1>startpage</h1>
+        </div>
+    )
+}
+
+export default Startpage
